bcead9ca533ee70b59451c08b2037721902c56d1
Bambi007 <dziedziochs@gmail.com>
2021-05-10 13:33:42 +0200

    representative letters added - need to fill the path to file

---
+++ src/components/cms-data/special-links-data.js
@@ -0,0 +1,20 @@
+import MGILogo from '../../assets/images/partners/mgi.png';
+import logo3 from "../../assets/images/partners/my-medikal.png";
+
+
+let specialLinksData = [
+  {
+    paragraph: 'Od 2020 jesteśmy wyłącznym dystrybutorem marki MGI Medical',
+    logo: MGILogo,
+    logoAlt: '',
+    file: '',
+  },
+  {
+    paragraph: 'Od 2021 roku jesteśmy wyłącznym dystrybutorem marki MyMedical',
+    logo: logo3,
+    logoAlt: '',
+    file: '',
+  }
+]
+
+export { specialLinksData };
---
+++ src/components/special-links.jsx
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+import { specialLinksData } from './cms-data/special-links-data';
+
+
+
+class SpecialLink extends Component {
+  render() {
+    return (
+      <div className="special-link">
+        <div className="special-link-logo">
+          <img className="special-link-logo-image" src={this.props.img} alt={this.props.alt} />
+        </div>
+        <div className="special-link-info">
+
+          <p>{this.props.paragraph}</p>
+
+          <a className='special-link-download small-button' href={this.props.path}>List przedstawicielski</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+const specialLinksSet = specialLinksData.map((element, index) =>
+  < SpecialLink key={index} paragraph={element.paragraph} img={element.logo} alt={element.alt} path={element.path} />
+)
+
+export class SpecialLinks extends Component {
+  render() {
+    return (
+
+      <section className='special-links'>
+        <div className="container">
+          {specialLinksSet}
+        </div>
+      </section>
+    )
+  }
+}
